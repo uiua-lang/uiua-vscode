9fa44618ea98d5c8feaf83ae9fd52c0516a5a9e8
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-02-12 22:24:06 -0800

    copy interpreter before running

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,25 @@
+import { execSync } from 'child_process';
 import * as vscode from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+import { Command, LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+import { chmodSync, copyFileSync } from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-	var path = vscode.workspace.getConfiguration('uiua').get<string>('executablePath') || 'uiua';
+	// Determine Uiua interpreter path
+	var path = vscode.workspace.getConfiguration('uiua').get<string>('executablePath');
+	path = path || (process.platform === 'win32'
+		? execSync('where uiua')
+		: execSync('which uiua')).toString().trim();
+	// Copy the interpreter to the extension's folder
+	var extensionPath = context.extensionPath;
+	var copyPath = extensionPath + '/uiua' + (process.platform === 'win32' ? '.exe' : '');
+	copyFileSync(path, copyPath);
+	if (process.platform !== 'win32') {
+		chmodSync(copyPath, 0o755);
+	}
+
+	// Start the language server
 	let serverOptions: ServerOptions = {
-		command: path,
+		command: copyPath,
 		args: ['lsp'],
 		options: {}
 	};
